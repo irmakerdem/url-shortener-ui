09d00933948968bc89abf361351e6326b8cd53c1
Irmak Erdem <irmakerdem9@gmail.com>
2022-08-09 10:52:26 -0600

    Add beforeEach and set up 3 it blocks

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,19 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('URL Shortener', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { fixture: "getMockData.json" })
+    cy.visit('http://localhost:3000/');
+    cy.contains('URL Shortener');
+  })
+
+  it('Should display two url cards on page load', () => {
+
+  })
+
+  it('Should display two inputs and a button', () => {
+
+  })
+
+  it('Should allow user to submit form and see new url card', () => {
+
   })
 })
