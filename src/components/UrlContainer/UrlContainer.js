73a639c007e1a0997d0b462a02523749489313cb
Irmak Erdem <irmakerdem9@gmail.com>
2022-08-09 09:21:53 -0600

    Add key to URL Container

--- src/components/UrlContainer/UrlContainer.js
+++ src/components/UrlContainer/UrlContainer.js
@@ -3,8 +3,9 @@ import './UrlContainer.css';
 
 const UrlContainer = props => {
   const urlEls = props.urls.map(url => {
+    console.log(url)
     return (
-      <div className="url">
+      <div className="url" key={url.id}>
         <h3>{url.title}</h3>
         <a href={url.short_url} target="blank">{url.short_url}</a>
         <p>{url.long_url}</p>
